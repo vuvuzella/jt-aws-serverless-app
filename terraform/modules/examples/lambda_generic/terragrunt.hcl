2b09817dcd6fc4c2d7d3d49eb15ef4360155f244
vuvuzella <j.tabacii@gmail.com>
2022-03-15 21:29:13 +1100

    added modules folder for terraform modules added terragrunt usage

---
+++ terraform/modules/examples/lambda_generic/terragrunt.hcl
@@ -0,0 +1,8 @@
+include "module_workspace" {
+  path = find_in_parent_folders()
+}
+
+inputs = {
+  app_artifact = "../../../../artifacts/app.zip"
+  app_dep_artifact = "../../../../artifacts/dependencies.zip"
+}
---
+++ terraform/modules/lambda_generic/variables.tf
@@ -0,0 +1,7 @@
+variable "app_artifact" {
+  description = "Path to the packaged application artifact"
+}
+
+variable "app_dep_artifact" {
+  description = "Path to the packaged application dependencies artifact"
+}
