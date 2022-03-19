0b2c8ed6f928406af914ae52b4753835b8ca2329
vuvuzella <j.tabacii@gmail.com>
2022-03-19 23:54:57 +1100

    fix circleci and tests

--- backend/api/app/tests/index.unit.test.ts
+++ backend/api/app/tests/index.unit.test.ts
@@ -5,7 +5,7 @@ describe('Unit testing for handlser', () => {
   it('Execute correctly', async () => {
     const result = await helloWorld();
     expect(result.statusCode).toBe(200);
-    expect(result.body).toBe('Hello World from Lambda!')
+    expect(result.body).toBe('"Hello World from Lambda!"')
   });
 
 });
